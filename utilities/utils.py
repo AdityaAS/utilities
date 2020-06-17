af3df6a1b4841b31c5ca8aa67c5c77d82d7aaea1
Apoorve Singhal <apoorve.singhal@wadhwaniai.org>
2020-06-17 15:55:53 +0000

    added fn for one_hot_vector

--- utilities/utils.py
+++ utilities/utils.py
@@ -7,3 +7,21 @@ def fig_to_numpy(fig):
     image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return image
 
+def make_one_hot(self, position: int, l: int) -> torch.Tensor:
+    """
+    Makes a 1D one hot vector of length l with a one at index position, rest all zeroes
+
+    Arguments:
+    position (int): index at which the one hot vector would have one
+    l (int): length of the one hot vector
+
+    Returns:
+    torch.Tensor
+
+    Usage:
+    make_one_hot(position=3, l=5) -> torch.Tensor([0, 0, 0, 1, 0])
+    """
+    one_hot_vec = torch.zeros(length)
+    one_hot_vec[position] = 1
+
+    return one_hot_vec
