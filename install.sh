3ea09bb61bae3a5235547a9166f849f4dd1686db
Aditya Sarma <aditya@wadhwaniai.org>
2020-06-19 03:00:24 +0530

    Adds code to clean up extra folders after installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-python -m pip uninstall utilities
+pip uninstall utilities
 python setup.py sdist bdist_wheel
-python -m pip install dist/utilities-0.0.1-py3-none-any.whl
-
+pip install dist/utilities-0.0.1-py3-none-any.whl
+rm -rf build dist utilities.egg-info
